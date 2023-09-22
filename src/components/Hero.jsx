b280eba9ce6e5e6e9fd9340db1d21ada35164a6c
Justin Tran <tranjg@mail.uc.edu>
2023-09-21 20:23:13 -0400

    feat: added scroll tooltip icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,14 +13,30 @@ const Hero = () => {
       </div>
 
       <div>
-        <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Justin Tran</span></h1>
+        <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Justin</span></h1>
         <p className={`${styles.heroSubText} mt-2 text-white`}>
-        Elevating digital experiences: Mastering UI/UX,<br className='sm:block hidden'/> forging web solutions, and expressing mobile innovation.
+        I develop digial experiences by mastering UI/UX, <br className='sm:block hidden'/> crafting web and mobile solutions.
         </p>
       </div>
     </div>
 
     <ComputersCanvas />
+    <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <a href='#about'>
+        <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <motion.dev 
+          animate={{
+            y: [0,24,0]
+          }}
+          transition={{
+            duration: 1.5,
+            repeat: Infinity,
+            repeatType: 'loop'
+          }}
+          className='w-3 h-3 rounded-full bg-secondary mb-1' />
+        </div>
+      </a>
+    </div>
     </section>
   )
 }
