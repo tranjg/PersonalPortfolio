8472209ca2e0e1b01c9240547572ab8a8891e2a0
Justin Tran <tranjg@mail.uc.edu>
2023-10-09 21:08:30 -0400

    refactor: temporarily removed unused elements: contact section, works section

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,12 @@ const App = () => {
       <About />
       <Experience />
       <Tech />
-      <Works />
+      {/* <Works />
       <Feedbacks />
       <div className="relative z-0">
         <Contact />
         <StarsCanvas />
-      </div>
+      </div> */}
     </div>
     </BrowserRouter>
   )
